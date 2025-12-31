f6b946384427a078b7911c7c7ed9a87a0757beb5
noma <125211901+khawlanoman@users.noreply.github.com>
2025-12-31 22:31:43 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -12,66 +12,61 @@
 
 #include "push_swap.h"
 
-void check_and_convert(t_stack *a,t_stack *b,char **res)
+int main(int argc, char **argv)
 {
+    t_stack *a;
+    t_stack *b;
+    t_stack *new_node;
+    int i;
     int j;
     int error;
     int p;
-    t_stack *new_node;
-
+    a = NULL;
+    b = NULL;
+    i = 1;
     j = 0;
     p = 0;
-    error = 0;
+    if (argc < 2)
+        return (0);
+    while (i < argc)
+    {
+        
+        char **res= ft_split(argv[i], ' ');
+        if (!res || !res[0])
+            return(free_and_write_error(&a, &b, res));
+        
+       j = 0;
         while (res[j])
         {
             long num = ft_atoi(res[j],&error);
             if (error || (check_valid_number(res[j]) == 0 ) ||check_duplicate(a, ft_atoi(res[j],&error))== 0 )
-                        return (free_and_write_error(&a, &b, res));
+                return(free_and_write_error(&a, &b, res));
             int number = (int)num;
             new_node = ft_stack_new(number);
             new_node->position = p++;
             ft_lstadd_back(&a,new_node);
             j++;
         }
-
-}
-int main(int argc, char **argv)
-{
-    t_stack *a;
-    t_stack *b;
-    char **res;
-    int i;
-   
-    a = NULL;
-    b = NULL;
-    i = 1;
-   
-    if (argc < 2)
-        return (0);
-    while (i < argc)
-    {
-        
-        res= ft_split(argv[i], ' ');
-        if (!res || !res[0])
-             return (free_and_write_error(&a, &b, res));
-        
-      //
-        check_and_convert(a,b,res);
         free_split(res);
         i++;
     }
     add_index_to_node(&a);
 
    if (ft_lstsize(a) == 2)
+    {
         sort2(a);
-   else if (ft_lstsize(a) == 3)
+    }
+    if (ft_lstsize(a) == 3)
+    {
         sort3(&a);
+    }
     else
     {
         check_and_push_to_b(&a, &b);  
         push_back_to_a(&a, &b); 
         final_rotate(&a);
     }
+
     free_stack(&a);
     free_stack(&b);
     return (0);
