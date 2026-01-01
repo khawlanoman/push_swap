1df75f585e457c00f2475d0190fa1226e0e1f05d
noma <125211901+khawlanoman@users.noreply.github.com>
2026-01-01 15:00:10 +0100

    Improve error handling in main.c
    Replaced error handling with write statements for better error reporting.

--- main.c
+++ main.c
@@ -33,14 +33,20 @@ int main(int argc, char **argv)
         
         char **res= ft_split(argv[i], ' ');
         if (!res || !res[0])
-            return(free_and_write_error(&a, &b, res));
+        {
+            write (2, "Error\n", 6);
+            return (0);
+        }
         
        j = 0;
         while (res[j])
         {
             long num = ft_atoi(res[j],&error);
             if (error || (check_valid_number(res[j]) == 0 ) ||check_duplicate(a, ft_atoi(res[j],&error))== 0 )
-                return(free_and_write_error(&a, &b, res));
+            {
+                write (2, "Error\n", 6);
+                        return (0);
+            }
             int number = (int)num;
             new_node = ft_stack_new(number);
             new_node->position = p++;
@@ -53,20 +59,27 @@ int main(int argc, char **argv)
     add_index_to_node(&a);
 
    if (ft_lstsize(a) == 2)
-    {
         sort2(a);
-    }
-    if (ft_lstsize(a) == 3)
-    {
+   else if (ft_lstsize(a) == 3)
         sort3(&a);
-    }
     else
     {
         check_and_push_to_b(&a, &b);  
         push_back_to_a(&a, &b); 
         final_rotate(&a);
     }
-
+   /* printf("\nstack a\n");
+    while (a)
+    {
+        printf(" index :%d :value:%d position:%d\n",a->index,a->num,a->position);
+        a = a->next;
+    }
+    printf("stack b\n");
+    while (b)
+    {
+        printf("index :%d :value:%d position:%d cost:%d move: %d\n",b->index,b->num,b->position, b->cost_b, b->move);
+        b = b->next;
+    }*/
     free_stack(&a);
     free_stack(&b);
     return (0);
